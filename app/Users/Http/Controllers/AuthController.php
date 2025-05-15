81caefb733d5db4cee3365d5c49a4e20bc6625a3
Golluméo <p.mauriello54@gmail.com>
2025-05-15 10:32:05 +0200

    feat(api): add authentication token endpoint
    Introduce the ` /auth / token ` POST endpoint handled by AuthController. It processes login requests using the Login request class and returns a JSON response. Enables basic structure for authentication functionality.

--- app/Users/Http/Controllers/AuthController.php
+++ app/Users/Http/Controllers/AuthController.php
@@ -4,4 +4,13 @@ declare(strict_types=1);
 
 namespace App\Users\Http\Controllers;
 
-final class AuthController {}
+use App\Users\Http\Requests\Login;
+use Illuminate\Http\JsonResponse;
+
+final class AuthController
+{
+    public function __invoke(Login $request): JsonResponse
+    {
+        return new JsonResponse('hello-world', 200, [], JSON_PRETTY_PRINT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Users\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/token', AuthController::class);
