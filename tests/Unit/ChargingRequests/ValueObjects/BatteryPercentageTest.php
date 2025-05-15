28c1dac5f77ce2b01943f94ff0e1121263948442
Golluméo <p.mauriello54@gmail.com>
2025-05-15 15:50:07 +0200

    test(BatteryPercentage): add unit test skeleton for validation cases
    Introduce test skeletons for validating battery percentage handling. Includes placeholders for edge cases: 0, 100, below 0, and above 100.

---
+++ tests/Unit/ChargingRequests/ValueObjects/BatteryPercentageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Unit: Battery Percentage', function () {
+    it('accepts 0 as a valid percentage', function () {
+        // TODO
+    });
+
+    it('accepts 100 as a valid percentage', function () {
+        // TODO
+    });
+
+    it('throws if percentage is less than 0', function () {
+        // TODO
+    });
+
+    it('throws if percentage is greater than 100', function () {
+        // TODO
+    });
+});
