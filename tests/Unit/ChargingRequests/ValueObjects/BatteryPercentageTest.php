6849e6d3e6a7b58e03f71c8757fb35a317246f91
Golluméo <p.mauriello54@gmail.com>
2025-05-16 15:22:29 +0200

    test(BatteryPercentage): implement unit tests for valid and invalid percentages
    Add tests to ensure `BatteryPercentage` accepts values between 0 and 100 inclusively and throws exceptions for invalid values outside this range.

--- tests/Unit/ChargingRequests/ValueObjects/BatteryPercentageTest.php
+++ tests/Unit/ChargingRequests/ValueObjects/BatteryPercentageTest.php
@@ -2,20 +2,24 @@
 
 declare(strict_types=1);
 
+use App\ChargingRequests\ValueObjects\BatteryPercentage;
+
 describe('Unit: Battery Percentage', function (): void {
     it('accepts 0 as a valid percentage', function (): void {
-        // TODO
+        $batteryPercentage = new BatteryPercentage(0);
+        expect($batteryPercentage->value())->toBe(0.0);
     });
 
     it('accepts 100 as a valid percentage', function (): void {
-        // TODO
+        $batteryPercentage = new BatteryPercentage(100);
+        expect($batteryPercentage->value())->toBe(100.00);
     });
 
     it('throws if percentage is less than 0', function (): void {
-        // TODO
+        expect(fn () => new BatteryPercentage(-1))->toThrow(InvalidArgumentException::class);
     });
 
     it('throws if percentage is greater than 100', function (): void {
-        // TODO
+        expect(fn () => new BatteryPercentage(101))->toThrow(InvalidArgumentException::class);
     });
 });
