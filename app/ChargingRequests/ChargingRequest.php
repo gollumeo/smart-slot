4ef5d34beefac7ac81d129331e3d2b494d1800a9
Golluméo <p.mauriello54@gmail.com>
2025-05-16 16:30:15 +0200

    feat(chargingRequests): add fromDomain factory method to ChargingRequest
    Introduce a static factory method `fromDomain` to create a ChargingRequest instance from domain objects. This enhances the encapsulation of domain logic and simplifies object creation.

--- app/ChargingRequests/ChargingRequest.php
+++ app/ChargingRequests/ChargingRequest.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace App\ChargingRequests;
 
+use App\ChargingRequests\ValueObjects\BatteryPercentage;
+use App\ChargingRequests\ValueObjects\ChargingRequestStatus;
+use App\ChargingRequests\ValueObjects\ChargingWindow;
 use Illuminate\Database\Eloquent\Model;
 
-final class ChargingRequest extends Model {}
+final class ChargingRequest extends Model
+{
+    public static function fromDomain(int $userId, BatteryPercentage $batteryPercentage, ChargingWindow $chargingWindow, ChargingRequestStatus $status): self
+    {
+        $instance = new self();
+        $instance->user_id = $userId;
+        $instance->battery_percentage = $batteryPercentage->value();
+        $instance->starts_at = $chargingWindow->start();
+        $instance->ends_at = $chargingWindow->end();
+        $instance->status = $status;
+
+        return $instance;
+    }
+}
