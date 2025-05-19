b5ddc2ad6a3445772f6d5bd84fc1b2536d4a905e
Golluméo <p.mauriello54@gmail.com>
2025-05-19 15:12:16 +0200

    feat(ChargingRequests): add method to fetch active charging request for a user
    Introduce a `for` method in `ChargingRequestView` to retrieve the active charging request for a given user using the `ChargingRequestRepository`. This enhances functionality for fetching user-specific charging requests.

--- app/ChargingRequests/Read/ChargingRequestView.php
+++ app/ChargingRequests/Read/ChargingRequestView.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 namespace App\ChargingRequests\Read;
 
-final class ChargingRequestView {}
+use App\ChargingRequests\ChargingRequest;
+use App\Contracts\ChargingRequestRepository;
+use App\Users\User;
+
+final readonly class ChargingRequestView
+{
+    public function __construct(private ChargingRequestRepository $repository) {}
+
+    public function for(User $user): ?ChargingRequest
+    {
+        return $this->repository->getActiveRequestFor($user);
+    }
+}
