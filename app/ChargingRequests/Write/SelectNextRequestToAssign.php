c440c8c519a73a2b3d7b6a1e645e450ea10540a1
Golluméo <p.mauriello54@gmail.com>
2025-05-19 15:42:56 +0200

    feat(charging-requests): add method to select next request to assign
    Introduce `SelectNextRequestToAssign` class to determine the next charging request based on start time and battery level priority. This enables better management of queued requests.

---
+++ app/ChargingRequests/Write/SelectNextRequestToAssign.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ChargingRequests\Write;
+
+use App\ChargingRequests\ChargingRequest;
+use Illuminate\Support\Collection;
+
+final class SelectNextRequestToAssign
+{
+    public function __invoke(Collection $queuedRequests): ?ChargingRequest
+    {
+        return $queuedRequests
+            ->sort(function (ChargingRequest $a, ChargingRequest $b) {
+                if ($a->starts_at->lt($b->starts_at)) {
+                    return -1;
+                }
+                if ($a->starts_at->gt($b->starts_at)) {
+                    return 1;
+                }
+
+                return $a->battery_percentage <=> $b->battery_percentage;
+            })
+            ->values()
+            ->first();
+    }
+}
--- app/ChargingRequests/Write/WaitingLine.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\ChargingRequests\Write;
-
-final class WaitingLine {}
