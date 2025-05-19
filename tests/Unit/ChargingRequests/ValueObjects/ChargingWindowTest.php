2863e951f3187ddd15b470885c422f41614515ad
Golluméo <p.mauriello54@gmail.com>
2025-05-19 16:42:16 +0200

    test(ChargingWindow): add return type hints to test cases
    Add `: void` return type hints to test case methods in `ChargingWindowTest.php` to improve code clarity and enforce explicit method return types.

--- tests/Unit/ChargingRequests/ValueObjects/ChargingWindowTest.php
+++ tests/Unit/ChargingRequests/ValueObjects/ChargingWindowTest.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 use App\ChargingRequests\ValueObjects\ChargingWindow;
 use Carbon\CarbonImmutable;
 
-describe('Unit: Charging Window', function () {
-    it('throws if it ends before it starts', function () {
+describe('Unit: Charging Window', function (): void {
+    it('throws if it ends before it starts', function (): void {
         $start = CarbonImmutable::now();
         $end = $start->subDay();
         expect(fn () => new ChargingWindow(startTime: $start, endTime: $end))->toThrow(InvalidArgumentException::class);
     });
 
-    it('throws if it starts as it ends', function () {
+    it('throws if it starts as it ends', function (): void {
         $now = CarbonImmutable::now();
         expect(fn () => new ChargingWindow(startTime: $now, endTime: $now))->toThrow(InvalidArgumentException::class);
     });
 
-    it('accepts a valid interval', function () {
+    it('accepts a valid interval', function (): void {
         $start = CarbonImmutable::createFromFormat('d-m-Y H:i', '16-05-2025 14:00', 'Europe/Brussels');
         $end = CarbonImmutable::createFromFormat('d-m-Y H:i', '16-05-2025 17:00', 'Europe/Brussels');
 
