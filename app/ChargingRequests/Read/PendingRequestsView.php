89f8f1595355aaaec6148bf555346c34a98582df
Golluméo <p.mauriello54@gmail.com>
2025-05-19 15:00:44 +0200

    feat(ChargingRequests): add method to fetch pending requests
    Introduce a `get` method in `PendingRequestsView` to retrieve pending charging requests using the `ChargingRequestRepository`. This enhances the capability to access pending requests directly.

--- app/ChargingRequests/Read/PendingRequestsView.php
+++ app/ChargingRequests/Read/PendingRequestsView.php
@@ -4,4 +4,15 @@ declare(strict_types=1);
 
 namespace App\ChargingRequests\Read;
 
-final class PendingRequestsView {}
+use App\Contracts\ChargingRequestRepository;
+use Illuminate\Support\Collection;
+
+final class PendingRequestsView
+{
+    public function __construct(private ChargingRequestRepository $repository) {}
+
+    public function get(): Collection
+    {
+        return $this->repository->getPendingRequests();
+    }
+}
