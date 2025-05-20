d1dace4151528fb88616a7c2899f727a892e3b1e
Golluméo <p.mauriello54@gmail.com>
2025-05-20 15:26:41 +0200

    feat(charging-slot-repository): add list method to repository interface
    Introduce a new `list` method in the `ChargingSlotRepository` interface to retrieve a collection of charging slots. This addition enhances the repository's capability for listing available charging slots.

--- app/Contracts/ChargingSlotRepository.php
+++ app/Contracts/ChargingSlotRepository.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Contracts;
 
 use App\ChargingSlots\ChargingSlot;
+use Illuminate\Support\Collection;
 
 interface ChargingSlotRepository
 {
-    public function findAvailableSlot(): ?ChargingSlot;
-
     public function save(ChargingSlot $slot): void;
+
+    /**
+     * @return Collection<int, ChargingSlot>
+     */
+    public function list(): Collection;
 }
