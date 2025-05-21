84437958d2f8adb58a6ee3feb4c7aa571d96aa1b
Golluméo <p.mauriello54@gmail.com>
2025-05-21 15:38:54 +0200

    test(ChargingRequests): implement unit test for canceling and reassigning requests
    Add test implementation for `CancelChargingRequest` use case to ensure cancellation of assigned requests successfully reassigns the charging slot to the next eligible request. Mock dependencies and validate functional behavior.

--- tests/Unit/ChargingRequests/CancelChargingRequestTest.php
+++ tests/Unit/ChargingRequests/CancelChargingRequestTest.php
@@ -9,6 +9,7 @@ use App\ChargingRequests\Write\AssignSlotToRequest;
 use App\ChargingRequests\Write\CancelChargingRequest;
 use App\ChargingRequests\Write\SelectNextRequestToAssign;
 use App\Contracts\ChargingRequestRepository;
+use Illuminate\Support\Collection;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
@@ -42,7 +43,41 @@ describe('Unit: Cancel Charging Request', function (): void {
     });
 
     it('cancels an assigned request and reassigns the slot', function (): void {
-        // TODO
+        /** @var TestCase $this */
+        $user = $this->createStaticTestUser();
+
+        $chargingRequest = ChargingRequest::fromDomain(
+            userId: $user->id,
+            batteryPercentage: new BatteryPercentage(50),
+            chargingWindow: $this->createWindow('21-05-2025 09:00', '21-05-2025 12:00'),
+            status: ChargingRequestStatus::ASSIGNED
+        );
+        $chargingRequest->slot_id = 42;
+
+        $nextRequest = ChargingRequest::fromDomain(
+            userId: $user->id,
+            batteryPercentage: new BatteryPercentage(10),
+            chargingWindow: $this->createWindow('21-05-2025 11:00', '21-05-2025 15:00'),
+        );
+
+        /** @var MockInterface&ChargingRequestRepository $repository */
+        $repository = $this->makeMock(ChargingRequestRepository::class);
+        $repository->shouldReceive('save')->once()->with($chargingRequest);
+        $repository->shouldReceive('getPendingRequests')->once()->andReturn(collect([$nextRequest]));
+
+        /** @var MockInterface&AssignSlotToRequest $assignSlot */
+        $assignSlot = $this->makeMock(AssignSlotToRequest::class);
+        $assignSlot->shouldReceive('__invoke')->once()->with($nextRequest);
+
+        /** @var MockInterface&SelectNextRequestToAssign $selectNextRequest */
+        $selectNextRequest = $this->makeMock(SelectNextRequestToAssign::class);
+        $selectNextRequest->shouldReceive('__invoke')->once()
+            ->with(Mockery::on(fn (Collection $collection) => $collection->contains($nextRequest)))
+            ->andReturn($nextRequest);
+
+        $useCase = new CancelChargingRequest($repository, $assignSlot, $selectNextRequest);
+
+        expect(fn () => $useCase->execute($chargingRequest))->not()->toThrow(Exception::class);
     });
 
     it('cannot cancel a terminal request', function (): void {
