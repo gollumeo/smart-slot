83e4b8f783f5b1d1ae6c24e260a946d951785b5f
Golluméo <p.mauriello54@gmail.com>
2026-05-05 11:59:01 +0200

    feat(seeders): add default user creation in DatabaseSeeder
    Introduce default user creation in the `DatabaseSeeder` for testing and development. Two users, Alice and Bob, are seeded with predefined names and emails. This ensures a consistent setup for local environments and developer convenience.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\ChargingSlots\ChargingSlot;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Users\User;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
@@ -16,5 +16,15 @@ final class DatabaseSeeder extends Seeder
     public function run(): void
     {
         ChargingSlot::factory(5)->create();
+
+        User::factory()->create([
+            'name' => 'Alice',
+            'email' => 'alice@test.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Bob',
+            'email' => 'bob@test.com',
+        ]);
     }
 }
